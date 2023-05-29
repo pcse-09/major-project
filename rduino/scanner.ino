6f590f012a1f170d5e2e1a1eaa67ec78f40ae8c2
pcse-09 <134901905+pcse-09@users.noreply.github.com>
2023-05-29 10:39:46 +0530

    Update scanner.ino

--- rduino/scanner.ino
+++ rduino/scanner.ino
@@ -1,1 +1,73 @@
-jbfhjerbfhjerbfjherbf
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+#define RST_PIN         D0          // Configurable, see typical pin layout above
+#define SS_PIN          D8         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("Xiaomi 11i", "12345678");
+	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
+
+void loop() {
+	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+  	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    
+    WiFiClient client;
+
+    HTTPClient http;
+    String tagID = "";
+    for ( uint8_t i = 0; i < 4; i++) { // The MIFARE PICCs that we use have 4 byte UID
+    //readCard[i] = mfrc522.uid.uidByte[i];
+    tagID.concat(String(mfrc522.uid.uidByte[i], HEX)); // Adds the 4 bytes in a single String variable
+    }
+    tagID.toUpperCase();
+    mfrc522.PICC_HaltA(); // Stop reading
+    Serial.println(tagID);
+
+    String baseUrl = "http://143.110.184.208:5001";
+    String endPoint = "/cards";
+    String params = "?cardId="+tagID;
+
+    String url = baseUrl+endPoint+params;
+
+     if (http.begin(client, url)){
+       int httpCode = http.GET();
+       if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+      http.end();
+     } else {
+      Serial.println("[HTTP] Unable to connect");
+    }
+  }
+}
